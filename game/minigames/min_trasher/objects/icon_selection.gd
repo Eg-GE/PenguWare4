e238388085af11fa413bfbd4147fc85fb90137ae
Eg-GE <unlukorayege@gmail.com>
2024-03-26 01:28:33 +0300

    icon labels done

--- game/minigames/min_trasher/objects/icon_selection.gd
+++ game/minigames/min_trasher/objects/icon_selection.gd
@@ -6,51 +6,50 @@ var ysize : float
 var xsize : float
 var xpos : float
 var ypos : float
-var labelglobalsize : Vector2
+
 var xlpos : float
 var ylpos : float
 @onready var selectobject : Sprite3D = $Selection
-@onready var labelobject : Label = $Label
+@onready var labelobject : Label3D = $Label
+@onready var pixelsize : float = labelobject.pixel_size
+@onready var fontsize : int = labelobject.font_size
 
 var parentglobalpos
-
-var linecount : int = 1
-var lineheight : float = .1
+var lineheight : float
 
 var margin = Vector3(0.2,0.2,0)
 
 func _ready():
 	selectobject.modulate.a = 0.0
-	linecount = labelobject.get_line_count()
-	lineheight = labelobject.get_line_height() / labelobject.label_settings.font_size
-	lineheight /= 2
+	lineheight = fontsize * pixelsize
 	print(lineheight)
 	
 	
 func _process(delta):
 	
-	
 	parentglobalpos = get_parent().global_transform.origin
 
-	
 	meshglobalaabb = meshobject.global_transform * meshobject.get_aabb()
 	xsize = meshglobalaabb.size.x + (margin.x*2)
-	ysize = meshglobalaabb.size.y + (margin.y*2) + (lineheight*linecount)
-	selectobject.scale.x = xsize
-	selectobject.scale.y = ysize
-	
+	ysize = meshglobalaabb.size.y + (margin.y*2) + lineheight
 	xpos = parentglobalpos.x
 	ypos = parentglobalpos.y - (ysize/2 - (margin.y + meshglobalaabb.size.y/2))
+	
+	
+	xlpos = parentglobalpos.x 
+	ylpos = parentglobalpos.y - meshglobalaabb.size.y/2
+	
+	
+	labelobject.position.x = xlpos
+	labelobject.position.y = ylpos
+	
+	selectobject.scale.x = xsize
+	selectobject.scale.y = ysize
 	
 	position.x = xpos
 	position.y = ypos
 	
-	labelglobalsize = labelobject.get_global_transform().origin
 	
-	
-	labelobject.position.x = xpos
-	labelobject.position.y = ypos
-
 func _on_rigidbody_input_event(camera, event, position, normal, shape_idx):
 	
 	pass 
